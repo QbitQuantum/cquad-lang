a6d499a69396651701223ff66321e8f75d4d8d14
QbitQuantum <billchifer@bk.ru>
2026-08-27 02:10:06 +0700

    Выносим в отдельный файл синатксис языка

--- c-quad.cpp
+++ c-quad.cpp
@@ -3,44 +3,28 @@
 
 #include <iostream>
 #include <string>
-
+#include <fstream>
 #include "Lexer.hpp"
 #include "PostLexer.hpp"
 #include "Parser.hpp"
 
-const std::string code =
-R"(
-class M
-{
-    // Одиночные объявления
-    int a = 10;
-    Data* data = nullptr;
-    std::vector<int> a;
+std::string ReadFile(std::string filepath) {
 
-    // Групповые объявления
-    int x, y, z;
-    const int[5] arr1, arr2, arr3;
-    Data* ptr1, ptr2 = nullptr, ptr3;
-
-    // Смешанные (с инициализацией)
-    int a = 10, b = 20, c;
-
-    // Объявление функции
-    int* func2();
-    // Объявление функции с телом
-    int* func() { int a; };
-}
-
-// Объявление инициализации функции вне класса
-int* M::func2()
-{
+    std::ifstream file(filepath, std::ios::binary | std::ios::ate);
+    if (!file)
+        return "";
+    auto size = file.tellg();
+    std::string content(size, '\0');
+    file.seekg(0);
+    file.read(&content[0], size);
+    return content;
 };
-
-)";
 
 int main()
 {
-	Lexer lexer(code);
+    std::string code = ReadFile("code.cqq");
+
+    Lexer lexer(code);
 	auto lexerbuffer = lexer.GetBufferLexerToken();
 
 	PostLexer postLexer(lexerbuffer);
