3c0e815d8ab8c1e1714cd0aaaa36918e7938d6ff
QbitQuantum <billchifer@bk.ru>
2026-09-10 21:41:15 +0700

    Исправляем инкапсуляцию позиции токена

--- Parser/Parser.hpp
+++ Parser/Parser.hpp
@@ -37,39 +37,35 @@ namespace typeinitialization
 class TokenStream
 {
     std::vector<Token> Buffer;
-
+    mutable size_t Pos = 0;
+    const Token eofToken = { TokenKind::neof, "", 0, 0 };
 public:
-    size_t Pos = 0;
-
     TokenStream() = default;
 
     explicit TokenStream(const std::vector<Token>& buf) : Buffer(buf), Pos(0) {
         skipTrivia();
     }
 
-    inline bool isTriviaToken() const noexcept {
+    bool isTriviaToken() const noexcept {
         const TokenKind& k = peek().type;
         return tok::isWhitespaceToken(k) || tok::isCommentToken(k);
     }
 
-    void skipTrivia() {
-        while (!eof() && isTriviaToken()) {
-            ++Pos;
-        }
+    void skipTrivia() const noexcept {
+        while (!eof() && isTriviaToken()) ++Pos;
     }
 
-    const Token& peek(size_t offset = 0) const {
-        static Token eofToken{ TokenKind::neof, "", 0, 0 };
+    const Token& peek(size_t offset = 0) const noexcept {
         size_t idx = Pos + offset;
         if (idx >= Buffer.size()) return eofToken;
         return Buffer[idx];
     }
 
-    bool eof() const {
+    bool eof() const noexcept {
         return Pos >= Buffer.size();
     }
 
-    bool match(TokenKind id) {
+    bool match(TokenKind id) const noexcept {
         if (peek().type == id) {
             ++Pos;
             skipTrivia();
@@ -78,7 +74,7 @@ public:
         return false;
     }
 
-    const Token& consume(TokenKind id) {
+    const Token& consume(TokenKind id) const noexcept {
         const Token& tok = peek();
         if (tok.type == id) {
             ++Pos;
@@ -88,11 +84,11 @@ public:
         return tok;
     }
 
-    size_t savePosition() const {
+    size_t savePosition() const noexcept {
         return Pos;
     }
 
-    void restorePosition(size_t pos) {
+    void restorePosition(size_t pos) const noexcept {
         Pos = pos;
     }
 };
